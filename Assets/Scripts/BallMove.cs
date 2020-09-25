40f35ea7df2eac3905f0453afcf117023a5565c2
coke-official <cokeofficial1886@gmail.com>
2020-09-26 06:09:04 +0900

    first commit

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    public bool startPlay;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        startPlay=true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 myPos;
+        if (startPlay){
+            rb.velocity = new Vector3(0,0,0);
+            myPos=new Vector3(0,0.2f,0);
+            this.transform.position = myPos;
+            rb.AddForce(20,0,0,ForceMode.Impulse);
+            startPlay=false;
+            Debug.Log(startPlay);
+        }
+        else if (transform.position.x>20 | transform.position.x<-20)
+        {
+            startPlay=true;
+            Debug.Log(startPlay);
+        }
+    }
+}
---
+++ Assets/Scripts/Player2Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Move : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0,0,500,ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0,0,-500,ForceMode.Impulse);
+        } 
+    }
+}
